546b045188017e1c9d70298c7398a998c2c04c91
THEETHAWACH  BANNAKAN <fordeawq234@gmail.com>
2026-01-09 20:00:58 +0700

    Add files via upload

---
+++ day-2/11_loop.js
@@ -0,0 +1,37 @@
+var num;
+
+for(num=1; num <=10; num++){
+    if(num == 5){
+        console.log("middle")
+        break;
+        //continue;
+        
+    }
+    console.log(num)
+}
+
+
+var i = 0;
+while(i <= 5){
+    console.log(i);
+    i++;
+}
+
+
+/*while(true){
+ console.log(i)
+   i++;
+}*/
+
+var a = 0;
+
+while(a <=10){
+    console.log('*'.repeat(a));
+    a++;
+}
+var j = 10;
+
+while(a){
+    console.log('*'.repeat(j));
+    j--;
+}
